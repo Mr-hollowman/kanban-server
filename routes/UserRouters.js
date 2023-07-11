c38886f96075749fb8d1c004c879e53e7ecd237f
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-11 22:29:58 +0530

    routes updates

--- routes/UserRouters.js
+++ routes/UserRouters.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/Users.controllers';
+import { signin, signup } from '../controllers/Users.controllers';
 
 const router = express.Router();
 
-router.post('/signup',signup);
+router.post('/signup', signup);
+router.post('/signin', signin)
 
 
 export default router
