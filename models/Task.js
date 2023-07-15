7e2d1974cb08626eaf5ca5b8c480ea7919abc89b
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-16 01:30:44 +0530

    updates

--- models/Task.js
+++ models/Task.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+const subTask = new mongoose.Schema({
+    title: String,
+    isActive: { type: Boolean, default: false },
+})
+
 const TaskSchema = new mongoose.Schema({
-    boardId: {type:mongoose.Schema.Types.ObjectId, ref:"Boards", required: true},
+    boardId: { type: mongoose.Schema.Types.ObjectId, ref: "Boards", required: true },
     title: { type: String, required: true },
     desc: { type: String },
     column: { type: String, required: true },
-    subTask: { type: Array }
+    subTask: {type: [subTask]}
 }, { timestamps: true })
 
 export default mongoose.model("Task", TaskSchema)
